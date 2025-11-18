7a6d0ca2f7117f2f54592b6d19b3d1d6b9a6e422
achyutag <achyutacloud@outlook.com>
2025-11-18 13:25:11 +0530

    new code 18 Nov_inc

--- Admin/AdminDashboard/src/services/InspectionAPIService.ts
+++ Admin/AdminDashboard/src/services/InspectionAPIService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Global_API_BASE from "./GlobalConstants";
 import { InspectionUpdateRequest } from "../components/types/InspectionUpdateRequest";
 
-const API_URL = Global_API_BASE + "/api/bookings/inspections";
+const API_URL = Global_API_BASE + "/api/bookings/inspections/all";
 
 const InspectionAPIService = {
   // Fetch all bookings for inspection, optionally filtered by status
