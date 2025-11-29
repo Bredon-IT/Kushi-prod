ca361ae33bedf5cca755a4d305a49f1776dde3f5
bredon-ai <bredon-ai@bredonit.com>
2025-11-30 01:33:46 +0530

    updated prod url

--- Admin/AdminDashboard/src/services/GlobalConstants.ts
+++ Admin/AdminDashboard/src/services/GlobalConstants.ts
@@ -1,4 +1,4 @@
-const Global_API_BASE = "https://ibz8q2h3fe.ap-south-1.awsapprunner.com";  // backend api url.
+const Global_API_BASE = "https://bmytsqa7b3.ap-south-1.awsapprunner.com";  // backend api url.
 
 //const Global_API_BASE = "http://localhost:8082";  // backend api url.
 
