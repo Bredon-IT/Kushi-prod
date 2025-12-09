d92c9357a762854999e6ddc4d5983b9c8b7a9625
Narayanaswamy <mrswamy1987@gmail.com>
2025-12-09 19:24:28 +0530

    changing api url

--- project/src/services/GlobalConstants.ts
+++ project/src/services/GlobalConstants.ts
@@ -1,8 +1,8 @@
 // AWS AppRunner Backend URL (Production)
-//const Global_API_BASE = "https://ibz8q2h3fe.ap-south-1.awsapprunner.com";
+const Global_API_BASE = "https://ibz8q2h3fe.ap-south-1.awsapprunner.com";
 
 // Local Development (uncomment for local testing)
-const Global_API_BASE = "http://localhost:8080";
+//const Global_API_BASE = "http://localhost:8080";
 
 //const Global_API_BASE = "http://localhost:8082";
 
